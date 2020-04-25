1a1a355fdec8f90ace9922fe3208377c159b34f2
KAT <katreadwell1@gmail.com>
2020-04-25 10:55:04 -0700

    got help from Diarmuid, successfully able to display dino2guess

--- index.js
+++ index.js
@@ -5,82 +5,81 @@
 const Word = require("./word");
 const inquirer = require("inquirer");
 
-
-
 let incorrectLetters = [];
 let correctLetters = [];
 let guessesLeft = 5;
-isDinoGuessed = false;
-let needNewWord = false;
-
-
-let letterArray = "abcdefghijklmnopqrstuvwxyz";
+let isDinoGuessed = false;
+let needNewWord = true;
 
 
 //logic of the game
-function jeopardy() {
+(function jeopardy() {
     if (needNewWord = true) {
         function generateNewWord() {
             let dinosaurs = ["triceratops", "tyrannosaurus rex", "velociraptor", "stegasaurus", "diplodocus", "allosaurus"];
             let randomDinoIndex = Math.floor(Math.random() * dinosaurs.length);
             let randomDinosaur = dinosaurs[randomDinoIndex];
             computerGeneratedWord = new Word(randomDinosaur);
+            console.log(computerGeneratedWord)
         }
         generateNewWord();
+        needNewWord = false;
     }
 
-    else{
-        inquirer
+    inquirer
         .prompt([
             {
                 type: "input",
                 message: "Guess a letter.",
                 name: "userinput"
             }
-    
+
         ])
         .then(function (input) {
-                    if (incorrectLetters.includes(input.userInput) || correctLetters.includes(input.userInput)) {
-                        console.log("Letter already guessed.");
-                    } else {
-                        let wordCheckArray = [];
-                        computerGeneratedWord.userGuess(input.userInput);
-                        computerGeneratedWord.objArray.forEach(wordCheckArray);
-                        if (computerGeneratedWord.join('') === isDinoGuessed.join('')) {
-                            console.log("Wrong");
-                            incorrectLetters.push(input.userInput);
-                            guessesLeft--;
-                        } else {
-                            console.log("Correct");
-                            correctLetters.push(input.userInput);
-                        }
-                        computerGeneratedWord.log();
-                        console.log("Guesses Left: " + guessesLeft);
-                        console.log("Letters Guesses: " + incorrectLetters.join(" ,"));
-    
-                        if (guessesLeft > 0) {
-                            jeopardy();
-                        } else {
-                            console.log("Try again!");
-                            restartGame();
-                        }
-                        function wordCheck(key) {
-                            wordCheckArray.push(key.guessed);
-                        }
-                    }
-    
-                })
-        } 
-    }else {
-            console.log("You know your dinosaurs!");
-            restartGame();
-        }
-    
-        function completeCheck(key) {
-            isWordComplete.push(key.guessed);
-        }
-    }
-}
+            if (incorrectLetters.includes(input.userinput) || correctLetters.includes(input.userinput)) {
+                console.log("Letter already guessed.");
+            } else {
+                let wordCheckArray = [];
+                computerGeneratedWord.userGuess(input.userinput);
+                computerGeneratedWord.objArray.forEach(wordCheckArray);
+
+                wordCheckArray.forEach(function(value, index) {
+                    // this run for every item in the arr
+                    console.log('The value at index ' + index + ', is ' + value);
+                });
+
+                if (computerGeneratedWord.join('') === isDinoGuessed.join('')) {
+                    console.log("Wrong");
+                    incorrectLetters.push(input.userinput);
+                    guessesLeft--;
+                } else {
+                    console.log("Correct");
+                    correctLetters.push(input.userinput);
+                }
+                computerGeneratedWord.log();
+                console.log("Guesses Left: " + guessesLeft);
+                console.log("Letters Guesses: " + incorrectLetters.join(" ,"));
+
+                if (guessesLeft > 0) {
+                    jeopardy();
+                } else {
+                    console.log("Try again!");
+                    restartGame();
+                }
+                function wordCheck(key) {
+                    wordCheckArray.push(key.guessed);
+                }
+            }
+            if (isDinoGuessed === true) {
+                console.log("You know your dinosaurs!");
+                needNewWord = true;
+                restartGame();
+            }
+
+        })
+})(); 
+
+
 
 function restartGame() {
     incorrectLetters = [];
